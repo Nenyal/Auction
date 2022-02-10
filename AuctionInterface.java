ae6a666dbd8c77d45480b705306c0331ea2b0786
Nenyal <92599257+Nenyal@users.noreply.github.com>
2022-02-11 01:54:14 +0300

    Delete AuctionInterface.java

--- AuctionInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-
-public interface AuktionInterface extends Remote {
-    
-    public String[] getAngebote() throws RemoteException;
-    
-    public void bid(String kundenname, int angebot_id, float preis) throws RemoteException;
-
-}
